cdeea5219f54985ab4008b18edd628ba05b66f24
dvitvitskiy.pro <dvitvitskiy.pro@gmail.com>
2017-07-01 20:12:04 +0300

    fix lens model show

--- app/controllers/api/v1/camera_models_controller.rb
+++ app/controllers/api/v1/camera_models_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::CameraModelsController < Api::V1::BaseController
   def index
-    respond_with(@camera_models = CameraModel.all)
+    # respond_with(@camera_models = CameraModel.joins(:camera_items))
+    # respond_with @camera_models.to_json(include: :camera_items)
+    respond_with @camera_models
   end
 end
--- app/controllers/api/v1/lens_models_controller.rb
+++ app/controllers/api/v1/lens_models_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::LensModelsController < Api::V1::BaseController
   def index
-    respond_with @lens_models.to_json(include: :lens_items)
-    # @lens_models = LensModel.includes(:lens_items)
-    # respond_with @lens_models
+    # respond_with(@lens_models = LensModel.joins(:lens_items))
+    # respond_with @lens_models.to_json(include: :lens_items)
+    respond_with @lens_models
   end
 end
