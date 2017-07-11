bb393694ae876ce0b43e989a0c62dfc6b50f0028
dvitvitskiy.pro <dvitvitskiy.pro@gmail.com>
2017-07-11 23:46:26 +0300

    new lensModel

--- db/migrate/20170701083907_add_fields_to_camera_model.rb
+++ db/migrate/20170701083907_add_fields_to_camera_model.rb
@@ -1,15 +1,15 @@
 class AddFieldsToCameraModel < ActiveRecord::Migration[5.0]
   def change
-    add_column :camera_models, :full_name, :string
-    add_column :camera_models, :dyxum_link, :string
-    add_column :camera_models, :dyxum_img_link, :string
-    add_column :camera_models, :release_year, :string
-    add_column :camera_models, :status, :string
-    add_column :camera_models, :viewfinder, :string
-    add_column :camera_models, :format, :string
+    add_column :camera_models, :full_name,        :string
+    add_column :camera_models, :dyxum_link,       :string
+    add_column :camera_models, :dyxum_img_link,   :string
+    add_column :camera_models, :release_year,     :string
+    add_column :camera_models, :status,           :string
+    add_column :camera_models, :viewfinder,       :string
+    add_column :camera_models, :format,           :string
     add_column :camera_models, :effective_pixels, :string
-    add_column :camera_models, :max_resolution, :string
-    remove_column :camera_models, :weather, :boolean
-    remove_column :camera_models, :stab, :string
+    add_column :camera_models, :max_resolution,   :string
+    remove_column :camera_models, :weather,       :boolean
+    remove_column :camera_models, :stab,          :string
   end
 end
